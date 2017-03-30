b538e38d4e997b93d5276721137ffbc274621f8a
Jonathan Gleason <jonathandgleason@gmail.com>
2017-03-30 11:52:50 -0600

    Fixed motd Gb/Mb

--- saved_config/zsh/motd.sh
+++ saved_config/zsh/motd.sh
@@ -1,6 +1,6 @@
 clear
 printf "          [0;34m.         "
-printf "[1;37m __ _ _ _ __|‾|_ [0;34m|‾(‾)_ _ _  ___ __"
+printf "[1;37m __ _ _ _ __| |_ [0;34m| ( )_ _ _  ___ __"
 printf "\n         [0;34m/#\\        "
 printf "[1;37m/ _\` | \'_/ _| \' \\[0;34m| | | \' \\ || \\ \\ /"
 printf "\n        [0;34m/###\       "
@@ -13,8 +13,9 @@ printf "[0;34mUptime:[0;37m "
 uptime -p | sed -e 's/^...//' | xargs echo -n
 printf "\n    [2;36m/###.   .###\     "
 printf "[0;34mDisk usage:[0;37m "
-df -h -t ext4 | grep / | awk '{print $3+0 " Gb/" $4+0 " Gb"}' | xargs echo -n
+df -h -t ext4 | grep / | awk '{print $3 "b / " $4 "b"}' | xargs echo -n
 printf "\n   [2;36m/#'         '#\    "
 printf "[0;34mRam:[0;37m "
-free -h | grep Mem | awk '{print $3+0 " Gb/" $2+0 " Gb"}' | xargs echo -n
+free -h | grep Mem | awk '{print $3 "b / " $2 "b"}' | xargs echo -n
 printf "\n"
+echo $used_space
