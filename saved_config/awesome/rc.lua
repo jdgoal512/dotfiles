5c3d9f476d1fbb11c965440306d85e10d9f8680f
jdgoal512 <jonathandgleason@gmail.com>
2019-04-25 10:22:33 -0600

    Changed awesome layouts and added lock screen to awesome

--- saved_config/awesome/rc.lua
+++ saved_config/awesome/rc.lua
@@ -74,9 +74,9 @@ modkey = "Mod1" --Mod1 is Alt, Mod4 is Windows
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.floating,
     -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
@@ -84,7 +84,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
+    -- awful.layout.suit.magnifier,
     awful.layout.suit.fullscreen,
     -- awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
@@ -413,6 +413,8 @@ globalkeys = gears.table.join(
               {description = "select next", group = "layout"}),
     awful.key({ "Mod4", "Shift"   }, "space", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
+    awful.key({ "Mod4",           }, ";", function () awful.spawn("i3lock") end,
+              {description = "lock screen", group = "launcher"}),
 
     awful.key({ modkey, "Control" }, "n",
               function ()
